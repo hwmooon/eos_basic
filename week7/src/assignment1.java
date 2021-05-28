ca4e9d02c40ac33653a0d7c449a7f81e6c531bbb
hwmooon <baekbingwol@gmail.com>
2021-05-28 12:45:49 +0900

    finally not working

--- week7/src/assignment1.java
+++ week7/src/assignment1.java
@@ -1,30 +1,29 @@
 import java.util.Scanner;
 
 public class assignment1 {
-    public static void call(int a) throws ArrayIndexOutOfBoundsException{
+    public static void call(int a) throws ArrayIndexOutOfBoundsException {
         int[] array = {0, 1, 2, 3};
-        if(a>3|| a<0)
+        if (a > 3 || a < 0)
             throw new ArrayIndexOutOfBoundsException();
-
-        System.out.println("call value : " + array[a]);
+        return array[a]
 
     }
+
     public static void main(String[] args) {
 
         try {
             Scanner sc = new Scanner(System.in);
             int a = sc.nextInt();
-            call(a);
-        }catch(ArrayIndexOutOfBoundsException e){
-            System.out.println(e);
+            System.out.println("call value : " + call(a));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("해당 문장은 무조건 수행");
         }
-        finally {
-            System.out.println("에러발생");
-        }
-    }
-    public static void arrayError() throws ArrayIndexOutOfBoundsException{
-
-            throw new ArrayIndexOutOfBoundsException();
-
     }
 }
+    class ArrayIndexOutOfBoundsException extends Exception {
+        public ArrayIndexOutOfBoundsException(){
+            super("에러발생");
+        }
+    }
